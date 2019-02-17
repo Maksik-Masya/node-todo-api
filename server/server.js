bd6f13ff9571c1362279ea0f0fbebd335c6fabc8
Maxym Borodenko <m.borodenko@aimprosoft.com>
2019-02-17 23:02:21 +0200

    Resource Creation Endpoint - POST

--- server/server.js
+++ server/server.js
@@ -1,60 +1,33 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
 
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((data) => {
+        res.send(data);
+        res.status(200);
+    }, (e) => {
+        res.status(400);
+        res.send(e);
+    });
+
 });
 
-const User = mongoose.model('User', {
-   email: {
-       type: String,
-       required: true,
-       trim: true,
-       minlength: 1
-   }
+
+
+
+app.listen(3000, () => {
+    console.log(`Starting up on port 3000`);
 });
-
-const newUser = new User({
-   email: 'test@email.com'
-});
-
-newUser.save().then((user) => {
-    console.log(`Saved user is: ${JSON.stringify(user, undefined, 2)}`)
-}, (err) => {
-    console.log('Unable to save the user', err);
-});
-
-// const newTodo = new Todo({
-//     text: "Cook dinner"
-// });
-//
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo: ', doc);
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
-
-// const newTodo = new Todo({
-//     text: '        Edit this video'
-// });
-//
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo: ', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save todo', e);
-// });
